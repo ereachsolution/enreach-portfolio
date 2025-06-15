fa405669b25cbf744357ba33856f46220fa5a9b6
Enreach Solution <contact.enreachsolution@gmail.com>
2025-06-15 18:44:23 +0530

    Fix

--- EnReach-Solutions-feature-deploy-test/vite.config.js
+++ EnReach-Solutions-feature-deploy-test/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: Process.env.VITE_BASE_PATH || "/enreach-portfolio",
+  
 })
